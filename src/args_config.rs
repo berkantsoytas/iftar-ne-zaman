13068633b794630ea2d26a241e7d064f880323df
SEFA1905 <70607438+SEFA1905@users.noreply.github.com>
2023-03-27 01:03:48 +0300

    args_config has been added

---
+++ src/args_config.rs
@@ -0,0 +1,35 @@
+
+
+pub struct Config {
+    pub city: String,
+    pub country: String,
+    pub region:String
+}
+
+//I saw that the api used has great minor precision
+//if "turkey ankara ankara" is written the api does not accept the request
+
+impl Config{
+    pub fn new(args: Vec<String>) -> Config {
+        if args.len() < 2 {
+            print!("Usage: {} [country] [region] [city]", args[0]);
+
+        }
+      
+        
+        //if the entered word is uppercase or lowercase, it is changed to all lowercase letters.
+        let country = args[1].clone().to_lowercase();
+        let region = args[2].clone().to_lowercase();
+        let city = args[3].clone().to_lowercase();
+    
+        Config { city,country,region}
+    }
+    //capitalizes the first letter of the content
+    pub fn capitalize_first_letter(&mut self)  {
+       self.city = self.city[0..1].to_ascii_uppercase()+&self.city[1..]; 
+       self.country = self.country[0..1].to_ascii_uppercase()+&self.country[1..]; 
+       self.region = self.region[0..1].to_ascii_uppercase()+&self.region[1..]; 
+
+       
+    }
+}
